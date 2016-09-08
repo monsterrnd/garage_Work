77bcc1e44779582b2115a226267b5d8829f64403
monsterrnd <monsterrnd@gmail.com>
2016-09-08 23:31:04 +0300

    админка

--- core/admin/user_list.php
+++ core/admin/user_list.php
@@ -5,62 +5,46 @@
 	</div>
 	<!-- /.col-lg-12 -->
 </div>
-<div class="col-lg-12">
-	<div class="panel panel-default">
-		<div class="panel-heading">
-			<button type="button" class="btn btn-success btn-xl"><i class="fa fa-plus"></i> Добавить</button>
-		</div>
-		<div class="panel-body">
-			<div class="table-responsive">
-				<table class="table table-bordered table-hover table-striped">
-					<thead>
-						<tr>
-							<th>Активность</th>
-							<th>Имя</th>
-							<th>Почта</th>
-							<th>Авто по умолчанию</th>
-							<th>Aватар</th>
-						</tr>
-					</thead>
-					<tbody>
-						<tr>
-							<td>1</td>
-							<td>12.09.2016 23:00</td>
-							<td>Замена масла</td>
-							<td>BMW X5 2016</td>
-							<td>VirbacAuto</td>
-							<td>1300Р</td>
-						</tr>                                                
 
-					</tbody>
-				</table>
-			</div>
-		</div>
-		<div class="panel-footer">
-			<nav aria-label="Page navigation" class="clearfix">
-			  <ul class="pagination pagination-sm navbar-right">
-				<li>
-				  <a href="#" aria-label="Previous">
-					<span aria-hidden="true">&laquo;</span>
-				  </a>
-				</li>
-				<li><a href="#">1</a></li>
-				<li><a href="#">2</a></li>
-				<li><a href="#">3</a></li>
-				<li><a href="#">4</a></li>
-				<li><a href="#">5</a></li>
-				<li>
-				  <a href="#" aria-label="Next">
-					<span aria-hidden="true">&raquo;</span>
-				  </a>
-				</li>
-			  </ul>
-			</nav>
-        </div>
-	</div>
+
+
+<div class="col-lg-12">
+<?
+
+	///запрос к пользователям
+	$users = new CUser;
+	$uers_list = $users->GetList();
 	
 	
-	<!-- /.table-responsive -->
+	$user_list = new CAdminTableList;
+
+	//// формеруем названия для шапки
+	$header = array(
+		"ID"=>"ID",
+		"ACTIVE"=>"Активность",
+		"PHONE"=>"Телефон",
+		"FIRST_NAME"=>"Имя",
+		"EMAIL"=>"Почта",
+		"MAIN_AUTO"=>"Авто по умолчанию",
+		"AVATAR"=>"Путь к аватарке",
+	);
+	$user_list->SetHeader($header);
+	/// записываем массив из результата в таблицу 
+	$user_list->SetData($uers_list);
+	// выводим таблицу
+	$user_list->Render();
+	
+	
+	
+	
+	
+
+?>
 </div>
+
+
+
+
+
 <?require($_SERVER["DOCUMENT_ROOT"]."/core/admin/footer.php");?>
 
